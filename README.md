2a516e11d32ccd45dfe04c232bb9c9b52b90c561
max <max@xyzmax.xyz>
2017-05-26 15:42:14 +0800

    Hello,world!

---
+++ README.md
@@ -0,0 +1,8 @@
+## java-gradle-console-example
+
+---
+
+## Usage
+
+    gradle build
+    gradle run
---
+++ src/main/java/Pirate/App.java
@@ -0,0 +1,9 @@
+package xyz.xyzmax.Pirate;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello,world");
+    }
+}
